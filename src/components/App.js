3f1144cfaa67eb33a069534f7239e608fa8d9013
Etienne Dldc <e.deladonchamps@gmail.com>
2019-05-05 12:36:27 +0200

    Add done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,20 @@ import React from 'react';
 class App extends React.Component {
   state = {
     newTodo: 'Todo 4',
-    todos: ['Todo 1', 'Todo 2', 'Todo 3'],
+    todos: [
+      {
+        name: 'Todo 1',
+        done: false,
+      },
+      {
+        name: 'Todo 2',
+        done: false,
+      },
+      {
+        name: 'Todo 3',
+        done: false,
+      },
+    ],
   };
 
   render() {
@@ -12,7 +25,27 @@ class App extends React.Component {
         <h1>Todo List</h1>
         <ul>
           {this.state.todos.map((todo, index) => {
-            return <li key={index}>{todo}</li>;
+            let textDeco = 'none';
+            if (todo.done === true) {
+              textDeco = 'line-through';
+            }
+            return (
+              <li
+                key={index}
+                style={{ textDecoration: textDeco }}
+                onClick={() => {
+                  // copier le tableau todos pour pouvoir le modifier
+                  const nextTodos = [...this.state.todos];
+                  // copier l'object todo dans nextTodos[index] pour pouvoir le modifier
+                  nextTodos[index] = { ...nextTodos[index] };
+                  // on peut maintenant modifier l'object car on l'a créer nous meme
+                  nextTodos[index].done = !nextTodos[index].done;
+                  this.setState({ todos: nextTodos });
+                }}
+              >
+                {todo.name}
+              </li>
+            );
           })}
         </ul>
         <input
@@ -24,7 +57,7 @@ class App extends React.Component {
         <button
           onClick={() => {
             const nextTodos = [...this.state.todos];
-            nextTodos.push(this.state.newTodo);
+            nextTodos.push({ name: this.state.newTodo, done: false });
             this.setState({ todos: nextTodos, newTodo: '' });
           }}
         >
