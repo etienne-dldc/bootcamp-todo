ea4bf56bf9601156ca5f028ab1eeee944abc43e3
Etienne Dldc <e.deladonchamps@gmail.com>
2020-03-15 16:59:29 +0100

    Update & convert to Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,68 @@
-import React from 'react';
+import React from "react";
 
-class App extends React.Component {
-  state = {
-    newTodo: 'Todo 4',
-    todos: [
-      {
-        name: 'Todo 1',
-        done: false,
-      },
-      {
-        name: 'Todo 2',
-        done: false,
-      },
-      {
-        name: 'Todo 3',
-        done: false,
-      },
-    ],
-  };
+const App = () => {
+  const [todos, setTodos] = React.useState([
+    {
+      name: "Todo 1",
+      done: false
+    },
+    {
+      name: "Todo 2",
+      done: false
+    },
+    {
+      name: "Todo 3",
+      done: false
+    }
+  ]);
+  const [newTodo, setNewTodo] = React.useState("Todo 4");
 
-  render() {
-    return (
-      <div>
-        <h1>Todo List</h1>
-        <ul>
-          {this.state.todos.map((todo, index) => {
-            let textDeco = 'none';
-            if (todo.done === true) {
-              textDeco = 'line-through';
-            }
-            return (
-              <li
-                key={index}
-                style={{ textDecoration: textDeco }}
-                onClick={() => {
-                  // copier le tableau todos pour pouvoir le modifier
-                  const nextTodos = [...this.state.todos];
-                  // copier l'object todo dans nextTodos[index] pour pouvoir le modifier
-                  nextTodos[index] = { ...nextTodos[index] };
-                  // on peut maintenant modifier l'object car on l'a créer nous meme
-                  nextTodos[index].done = !nextTodos[index].done;
-                  this.setState({ todos: nextTodos });
-                }}
-              >
-                {todo.name}
-              </li>
-            );
-          })}
-        </ul>
-        <input
-          value={this.state.newTodo}
-          onChange={event => {
-            this.setState({ newTodo: event.target.value });
-          }}
-        />
-        <button
-          onClick={() => {
-            const nextTodos = [...this.state.todos];
-            nextTodos.push({ name: this.state.newTodo, done: false });
-            this.setState({ todos: nextTodos, newTodo: '' });
-          }}
-        >
-          Add
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo, index) => {
+          let textDeco = "none";
+          if (todo.done === true) {
+            textDeco = "line-through";
+          }
+          return (
+            <li
+              key={index}
+              style={{ textDecoration: textDeco }}
+              onClick={() => {
+                // copier le tableau todos pour pouvoir le modifier
+                const nextTodos = [...todos];
+                // copier l'object todo dans nextTodos[index] pour pouvoir le modifier
+                nextTodos[index] = { ...nextTodos[index] };
+                // on peut maintenant modifier l'object car on l'a créer nous meme
+                nextTodos[index].done = !nextTodos[index].done;
+                setTodos(nextTodos);
+              }}
+            >
+              {todo.name}
+            </li>
+          );
+        })}
+      </ul>
+      <input
+        value={newTodo}
+        onChange={event => {
+          setNewTodo(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const nextTodos = [...todos];
+          nextTodos.push({ name: newTodo, done: false });
+          setTodos(nextTodos);
+          setNewTodo("");
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
 
 export default App;
